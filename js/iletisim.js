a0a9bfc72f975e068aa332f604cf9aef02f70e64
emreharman <emre.harman@ogr.sakarya.edu.tr>
2020-04-25 23:23:06 +0300

    Bootstrap eklenerek projenin yeniden düzenlenmesi. Signed-off-by: emreharman <emre.harman@ogr.sakarya.edu.tr>

--- js/iletisim.js
+++ js/iletisim.js
@@ -1,37 +1,37 @@
 function formDenetle(){
     var Ad=document.getElementById('ad').value;
-    var labelAd=document.getElementById('lblAd');
+    var labelAd=document.getElementById('labelAd');
     var Soyad=document.getElementById('soyad').value;
-    var labelSoyad=document.getElementById('lblSoyad');
+    var labelSoyad=document.getElementById('labelSoyad');
     var Mesaj=document.getElementById('mesaj').value;
-    var labelMesaj=document.getElementById('lblMesaj');
+    var labelMesaj=document.getElementById('labelMesaj');
     var Hata=document.getElementById('hata');
     if(!(isNaN(Ad))){
         labelAd.style.color="rgb(201, 55, 19)";
-        Hata.style.display="block";
+        Hata.style.visibility="visible";
         //return false;
     }
     else{
         labelAd.style.color="black";
-        Hata.style.display="none";
+        Hata.style.visibility="hidden";
     }
     if(!(isNaN(Soyad))){
         labelSoyad.style.color="rgb(201, 55, 19)";
-        Hata.style.display="block";
+        Hata.style.visibility="visible";
         //return false;
     }
     else{
         labelSoyad.style.color="black";
-        Hata.style.display="none";
+        Hata.style.visibility="hidden";
     }
     if(Mesaj==""){
         labelMesaj.style.color="rgb(201, 55, 19)";
-        Hata.style.display="block";
+        Hata.style.visibility="visible";
         //return false;
     }
     else{
         labelMesaj.style.color="black";
-        Hata.style.display="none";
+        Hata.style.visibility="hidden";
     }
     if(isNaN(ad) && isNaN(Soyad) && Mesaj != ""){
         return true;
--- js/login.js
+++ js/login.js
@@ -1,23 +1,21 @@
-
-function login(){
+function loginDenetle(){
     var kulAd="b191210049";
     var sifre="12345";
-    var kullaniciAdi=document.getElementById('kulAdi').value;
-    var Parola=document.getElementById('parola').value;
-    var Hata=document.getElementById('hata');
-    
-   if(kullaniciAdi=="" || Parola==""){
-       Hata.style.visibility="visible";
-       Hata.innerHTML="Kullanıcıadı ya da Parola boş bırakılamaz";
-   }
-   if((kullaniciAdi!="" && kullaniciAdi!=kulAd) || (Parola!="" && Parola!=sifre)){
-    Hata.style.visibility="visible";
-    Hata.innerHTML="Kullanıcıadı ya da Parola yanlış";
-   }
-   if((kullaniciAdi!="" && kullaniciAdi==kulAd) || (Parola!="" && Parola==sifre)){
-    Hata.style.visibility="visible";
-    Hata.innerHTML="Hoşgeldin "+kulAd;
-   }
-   
-   
+    var KullaniciAdi=document.getElementById("kullaniciAdi").value;
+    var Parola=document.getElementById("parola").value;
+    var Hata=document.getElementById("hata");
+
+    if(KullaniciAdi=="" || Parola==""){
+        Hata.style.visibility="visible";
+        Hata.innerHTML="Kullanıcı adı ya da Parola boş bırakılamaz.";
+        return false;
+    }
+    else if((KullaniciAdi!="" && KullaniciAdi!=kulAd) || (Parola!="" && Parola!=sifre)){
+        Hata.style.visibility="visible";
+        Hata.innerHTML="Kullanıcı adı ya da Parola yanlış.";
+        return false;
+    }
+    else{
+        return true;
+    }
 }
